5a87e8ffc5461d23e872420de5615c20dae79769
Ndumiso Mguni <Ndumiso29@gmail.com>
2020-03-02 22:49:17 -0500

    state added to nav and header

--- src/containers/nav.js
+++ src/containers/nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link, Route } from "react-router-dom";
 import { Jobs } from "./jobs";
 import { NewStories } from "./newStories";
@@ -7,75 +7,101 @@ import { Signin } from "./signin";
 
 import HeaderStyles from "../styles/header.module.css";
 
-const Nav = () => {
-  return (
-    <div>
-      <nav className={HeaderStyles.header}>
-        <h3 className={HeaderStyles.title}>
-          Hacker News <span className={HeaderStyles.titleApi}>API</span>
-        </h3>
-        <ul className={HeaderStyles.navList}>
-          <li>
-            {" "}
-            <Link
-              className={HeaderStyles.navItem}
-              activeClassName={HeaderStyles.activeNavItem}
-              to="/"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link
-              className={HeaderStyles.navItem}
-              activeClassName={HeaderStyles.activeNavItem}
-              to="/newStories"
-            >
-              New Stories
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link
-              className={HeaderStyles.navItem}
-              activeClassName={HeaderStyles.activeNavItem}
-              to="/jobs"
-            >
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: "API" };
+  }
+
+  changeApiTetx = () => {
+    this.setState({ title: "Stories" });
+  };
+  changeApiTetxJobs = () => {
+    this.setState({ title: "Jobs" });
+  };
+  changeApiTetxComents = () => {
+    this.setState({ title: "Comments" });
+  };
+
+  changeApiTetxSignin = () => {
+    this.setState({ title: "Signin" });
+  };
+
+  render() {
+    return (
+      <div>
+        <nav className={HeaderStyles.header}>
+          <h3 className={HeaderStyles.title}>
+            Hacker News <span>{this.state.title}</span>
+          </h3>
+
+          <ul className={HeaderStyles.navList}>
+            <li>
               {" "}
-              Jobs{" "}
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link
-              className={HeaderStyles.navItem}
-              activeClassName={HeaderStyles.activeNavItem}
-              to="/comments"
-            >
-              Comments
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link
-              className={HeaderStyles.navItem}
-              activeClassName={HeaderStyles.activeNavItem}
-              to="/signin"
-            >
+              <Link
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
               {" "}
-              Sign In
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Route path="/jobs" component={Jobs} />
-      {/* <Route path="/" component={StoriesContainer} /> */}
-      <Route path="/newStories" component={NewStories} />
-      <Route path="/comments" component={Comments} />
-      <Route path="/signin" component={Signin} />
-    </div>
-  );
-};
+              <Link
+                onClick={this.changeApiTetx}
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+                to="/newStories"
+              >
+                New Stories
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link
+                onClick={this.changeApiTetxJobs}
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+                to="/jobs"
+              >
+                {" "}
+                Jobs{" "}
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link
+                onClick={this.changeApiTetxComents}
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+                to="/comments"
+              >
+                Comments
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link
+                onClick={this.changeApiTetxSignin}
+                className={HeaderStyles.navItem}
+                activeClassName={HeaderStyles.activeNavItem}
+                to="/signin"
+              >
+                {" "}
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/jobs" component={Jobs} />
+        {/* <Route path="/" component={StoriesContainer} /> */}
+        <Route path="/newStories" component={NewStories} />
+        <Route path="/comments" component={Comments} />
+        <Route path="/signin" component={Signin} />
+      </div>
+    );
+  }
+}
 
 export default Nav;
